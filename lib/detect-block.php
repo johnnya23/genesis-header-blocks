d3d82b9c4627a9ce3b3851745cfb797e5565bb65
johnnya23 <johnantonacci23@gmail.com>
2021-04-07 20:02:31 -0600

    finish detect for kadense

--- lib/detect-block.php
+++ lib/detect-block.php
@@ -16,41 +16,54 @@ function jma_ghb_detect_block($args)
     global $post;
 
     $defaults = array(
+        'blocks' => array(),
         'name' => '',
         'post' => $post,
         'key' => '',
-        'value' => ''
+        'value' => '',
+        'drill' => 0
     );
     $args = wp_parse_args($args, $defaults);
 
-    if (is_int($args['post'])) {
-        $args['post'] = get_post($args['post']);
+    $blocks = $args['blocks'];
+
+    if (!count($blocks)) {
+        if (isset($args['post']) && function_exists('has_blocks')) {
+            if (is_int($args['post'])) {
+                $args['post'] = get_post($args['post']);
+            }
+            if (has_blocks($args['post']->post_content)) {
+                $blocks = parse_blocks($args['post']->post_content);
+            }
+        }
     }
-
-
-    if (is_object($args['post']) && function_exists('has_blocks') && has_blocks($args['post']->post_content)) {
-        $blocks = parse_blocks($args['post']->post_content);
-
-        if (is_array($blocks)) {
-            foreach ($blocks as $block) {
-                //recursively run reusable blocks
-                if ('core/block' == $block['blockName'] && isset($block['attrs']['ref']) && $block['attrs']['ref']) {
-                    $subargs = $args;
-                    $subargs['post'] = $block['attrs']['ref'];
-                    if (jma_ghb_detect_block($subargs)) {
-                        return true;
-                    }
+    if (count($blocks)) {
+        foreach ($blocks as $block) {
+            //recursively run reusable blocks
+            if (isset($block['blockName']) && 'core/block' == $block['blockName'] && isset($block['attrs']['ref']) && $block['attrs']['ref']) {
+                $subargs = $args;
+                $subargs['post'] = $block['attrs']['ref'];
+                if (jma_ghb_detect_block($subargs)) {
+                    return true;
                 }
-                if (!$args['name'] || $args['name'] == $block['blockName']) {
-                    //if value is set for $args['key'] or $args['value'] we require a match
-                    if ($args['key'] || $args['value']) {
-                        if (isset($block['attrs'][$args['key']]) && $block['attrs'][$args['key']] == $args['value']) {
-                            return true;
-                        }
-                    } elseif ($args['name'] && $args['name'] == $block['blockName']) {
-                        // else matching the block name is good enough
+            }
+            //recursively run inner blocks (if drill)
+            if ($args['drill'] && isset($block['innerBlocks']) && count($block['innerBlocks'])) {
+                $innerargs = $args;
+                $innerargs['blocks'] = $block['innerBlocks'];
+                if (jma_ghb_detect_block($innerargs)) {
+                    return true;
+                }
+            }
+            if (!$args['name'] || (isset($block['blockName']) && $args['name'] == $block['blockName'])) {
+                //if value is set for $args['key'] or $args['value'] we require a match
+                if ($args['key'] || $args['value']) {
+                    if (isset($block['attrs'][$args['key']]) && $block['attrs'][$args['key']] == $args['value']) {
                         return true;
                     }
+                } elseif ($args['name'] && $args['name'] == $block['blockName']) {
+                    // else matching the block name is good enough
+                    return true;
                 }
             }
         }
